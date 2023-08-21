4e1e7e56576da0d4e328d878dcb362f90b95d8ba
Aminaked <127684935+Aminaked@users.noreply.github.com>
2023-08-21 21:13:00 +0100

    Add files via upload

---
+++ project-tracking-intro-component-master-desktop/app.js
@@ -0,0 +1,17 @@
+const menuButton = document.getElementById("menuButton")
+
+const navbar = document.querySelector("nav")
+const closeMenu = document.querySelector(".close")
+const openMenu = document.querySelector(".menu")
+
+menuButton.addEventListener("click", () => {
+  if (navbar.classList.contains("open")) {
+    navbar.classList.remove("open")
+    closeMenu.style.display = "none"
+    openMenu.style.display = "block"
+  } else {
+    navbar.classList.add("open")
+    closeMenu.style.display = "block"
+    openMenu.style.display = "none"
+  }
+})
